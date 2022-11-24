3f49c1014a140798df87c4295bb160a1cd9a7fd4
Pchaozz <polsh2001.1@gmail.com>
2022-11-24 16:05:46 +0100

    Creado KingOfMelones

---
+++ KingOfMelones/src/main/java/KingOfMelones/KingOfMelonesApplication.java
@@ -0,0 +1,13 @@
+package KingOfMelones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingOfMelonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingOfMelonesApplication.class, args);
+	}
+
+}
---
+++ KingOfMelones/src/test/java/KingOfMelones/KingOfMelonesApplicationTests.java
@@ -0,0 +1,13 @@
+package KingOfMelones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingOfMelonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
