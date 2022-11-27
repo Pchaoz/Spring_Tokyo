5d50e596476856ff9b0e8683d8c8c3473fdadb89
jfuentesp <josefupe91@gmail.com>
2022-11-27 17:51:56 +0100

    Modelos creados

--- KingOfMelones/src/main/java/KingOfMelones/Model/Jugador.java
+++ KingOfMelones/src/main/java/KingOfMelones/Model/Jugador.java
@@ -1,5 +1,71 @@
 package KingOfMelones.Model;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "jugadors")
 public class Jugador {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_Jugador", nullable = false)
+	private int jugID;
+
+	@Column(name = "Nom", length = 50 , nullable = false)
+	private String Nom;
+
+	@Column(name = "Cognom", length = 50 , nullable = false)
+	private String Cognom;
+
+	@OneToMany(mappedBy="jugador", cascade = CascadeType.ALL)
+	private Set<Monstre> monstres;
+	
+	public Jugador() {
+		super();
+	}
+	
+	public Jugador(String nom, String cognom) {
+		super();
+		this.Nom = nom;
+		this.Cognom = cognom;
+	}
+
+	public int getJugID() {
+		return jugID;
+	}
+
+	public void setJugID(int jugID) {
+		this.jugID = jugID;
+	}
+
+	public String getNom() {
+		return Nom;
+	}
+
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+
+	public String getCognom() {
+		return Cognom;
+	}
+
+	public void setCognom(String cognom) {
+		Cognom = cognom;
+	}
+
+	@Override
+	public String toString() {
+		return "Jugador [jugID=" + jugID + ", Nom=" + Nom + ", Cognom=" + Cognom + "]";
+	}
+	
 }
--- KingOfMelones/src/main/java/KingOfMelones/Model/Partida.java
+++ KingOfMelones/src/main/java/KingOfMelones/Model/Partida.java
@@ -1,5 +1,66 @@
 package KingOfMelones.Model;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "partides")
 public class Partida {
 
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_Partida")
+	private int partidaID;
+	
+	@Column(name = "Torn")
+	private int Nom;
+
+	@Column(name = "Numero Jugadors",length=1)
+	private int Njugadors;
+
+	@OneToMany(mappedBy="partida", cascade = CascadeType.ALL)
+	private Set<Monstre> monstres;
+	
+	public int getPartidaID() {
+		return partidaID;
+	}
+
+	public void setPartidaID(int partidaID) {
+		this.partidaID = partidaID;
+	}
+
+	public int getNom() {
+		return Nom;
+	}
+
+	public void setNom(int nom) {
+		Nom = nom;
+	}
+
+	public int getNjugadors() {
+		return Njugadors;
+	}
+
+	public void setNjugadors(int njugadors) {
+		Njugadors = njugadors;
+	}
+
+	@Override
+	public String toString() {
+		return "Partida [partidaID=" + partidaID + ", Nom=" + Nom + ", Njugadors=" + Njugadors + "]";
+	}
+
+	
+	
+	
 }
