1484273148a5aa21bdbaebf569e04df692f2da2c
jfuentesp <josefupe91@gmail.com>
2022-11-27 17:46:46 +0100

    Services creados

--- KingOfMelones/src/main/java/KingOfMelones/Services/PartidaServices.java
+++ KingOfMelones/src/main/java/KingOfMelones/Services/PartidaServices.java
@@ -1,5 +1,36 @@
 package KingOfMelones.Services;
 
-public class PartidaServices {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import KingOfMelones.Model.Partida;
+import KingOfMelones.Repositoris.PartidaRepository;
+
+@Service
+public class PartidaServices {
+	@Autowired
+	PartidaRepository repository;
+	public Partida findById(Integer id) {
+		return repository.findById(id).orElse(null);
+	}
+	
+	public List<Partida> findAll() {
+		return repository.findAll();
+	}
+	
+	public void delete(Integer id) {
+
+		repository.deleteById(id);
+	}
+	
+	public Partida insertar(Partida p) {
+		return repository.save(p);
+	}
+	
+	public Partida editar(Partida p) {
+		return repository.save(p);
+	}
+	
 }
